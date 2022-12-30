27131db9779929144d290eb0d5fefe0b22812d59
theadg <deguzmanbernardandrew@gmail.com>
2022-12-30 20:43:32 +0800

    Finalize mergeSort.js

--- mergeSort.js
+++ mergeSort.js
@@ -2,25 +2,28 @@ const mergeSort = (arr) => {
   // divide into two
   if (arr.length === 2) {
     // sort the array
-    if (arr[1] < arr[0]) {
-      const sampleElement = arr[0];
-      arr[0] = arr[1];
-      arr[1] = sampleElement;
-    }
-
-    console.log(`SORTED: ${arr}`);
-    return arr;
+    return arr.sort(sortArray);
   }
 
   const left = arr.slice(0, arr.length / 2);
   const right = arr.slice(arr.length / 2, arr.length);
 
+  // recursive call to sort the arrays
   mergeSort(left);
   mergeSort(right);
 
-  console.log('LEFT', left);
-  console.log('RIGHT', right);
-  return { left, right };
+  // merge the arrays together
+  return merge(left, right);
+};
+
+function sortArray(a, b) {
+  return a - b;
+}
+
+const merge = (left, right) => {
+  const mergedArray = left.sort(sortArray).concat(right.sort(sortArray));
+
+  return mergedArray.sort(sortArray);
 };
 
 console.log(mergeSort([2, 1, 5, 4, 101, 100, 21, 19]));
